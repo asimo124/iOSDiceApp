2993a8a4017e6d4ea93c70f0bbc7f9f3656224fa
alex hawley <alex@engage2learn.org>
2021-01-27 18:26:15 -0600

    - Working on roll method

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgDice1: UIImageView!
+    @IBOutlet weak var imgDice2: UIImageView!
+    @IBOutlet weak var btnRoll: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imgDice1.image = #imageLiteral(resourceName: "DiceThree")
+        imgDice2.image = #imageLiteral(resourceName: "DiceFour")
     }
 
 
